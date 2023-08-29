5f0aebd0fe9724254c7b46494b75736e94e30035
sammit badodekar <badodekarsammit@gmail.com>
2023-08-29 20:01:41 +0530

    first commit

---
+++ components/ui/ThemeToggleButton.tsx
@@ -0,0 +1,32 @@
+import { BsFillMoonStarsFill, BsFillSunFill } from "react-icons/bs";
+import { useTheme } from "next-themes";
+
+const ThemeToggleButton = () => {
+  const { theme, setTheme } = useTheme();
+  console.log(theme);
+  return (
+    <div className=" flex items-center gap-2 text-xl  text-slate-500 dark:text-slate-200">
+      <div
+        className="relative  flex gap-4 rounded-full bg-slate-300 p-2 text-sm text-darkTheme dark:bg-slate-600 dark:text-slate-300"
+        onClick={() => {
+          if (theme === "dark") {
+            setTheme("light");
+          } else setTheme("dark");
+        }}
+      >
+        <button>
+          <BsFillSunFill />
+        </button>
+        <button>
+          <BsFillMoonStarsFill />
+        </button>
+        <div
+          className={`absolute top-1 h-6 w-6 rounded-full bg-blue-950 duration-300 ease-out dark:bg-yellow-300 ${
+            theme === "dark" ? " translate-x-6" : " -translate-x-1"
+          }`}
+        ></div>
+      </div>
+    </div>
+  );
+};
+export default ThemeToggleButton;
