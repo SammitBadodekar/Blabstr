f73ec339c88b436252f9af6a4886d163242a6c0e
sammit badodekar <badodekarsammit@gmail.com>
2023-09-04 18:26:44 +0530

    fixed deployment erorr

--- app/api/users/[email]/route.ts
+++ app/api/users/[email]/route.ts
@@ -1,18 +1,9 @@
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/db";
 
-export const GET = async ({ email }: { email: string }) => {
-  console.log(email);
+export const GET = async () => {
   try {
-    const user = await prisma.users.findFirst({
-      where: {
-        email: email,
-      },
-    });
-    return new NextResponse(JSON.stringify(user));
+    return new NextResponse(JSON.stringify("hi"));
   } catch (error) {
-    return new NextResponse("error occurred while getting user", {
-      status: 404,
-    });
+    return new NextResponse("error");
   }
 };
