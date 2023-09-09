4b49f8d770943124d2c1b71bdde375d104048c09
sammit badodekar <badodekarsammit@gmail.com>
2023-09-09 18:54:05 +0530

    test1 : remove cashe form api route

--- app/api/post/getMany/route.ts
+++ app/api/post/getMany/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/db";
 
+export const revalidate = 10;
+
 export const GET = async () => {
   try {
     const user = await prisma.posts.findMany({
