fa63e107a0428ec225114b5f388aabd338182b25
sammit badodekar <badodekarsammit@gmail.com>
2023-10-06 16:03:22 +0530

    fix: Link in communities

--- app/communities/page.tsx
+++ app/communities/page.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import Link from "next/link";
+import toast from "react-hot-toast";
 
 const Page = () => {
   return (
     <div className=" flex flex-col gap-4 p-4 sm:p-8">
-      <Link href="/new" className=" rounded-full text-lg font-extrabold">
+      <Link
+        href="/communities"
+        className=" w-full rounded-full border-2 bg-darkTheme p-2 px-4 text-lg font-extrabold text-lightTheme dark:bg-lightTheme dark:text-darkTheme"
+        onClick={() => toast("communities is in development")}
+      >
         Create a new community
       </Link>
     </div>
